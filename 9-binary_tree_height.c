97586781938577929cf91b63d4aee2dc5e423d96
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-04-11 13:54:23 +0100

    correct spelling error

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,5 +16,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (left_h > height_r ? left_h : right_h);
+	return (left_h > right_h ? left_h : right_h);
 }
