b1a8597b483055a8e2f876f1d37779cfa4803f79
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-04-11 13:49:43 +0100

    updated binary_trees.h

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_h = 0;
 	size_t right_h = 0;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
 	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
